4530fc2dc578db3656cd6ba294248ca0366a8ad0
Andrei Popovici <me@andreipopovici.co.uk>
2025-08-16 13:35:45 +0100

    Added some utils used for string manipulation

---
+++ frontend/src/utils/string.ts
@@ -0,0 +1,42 @@
+export function htmlEscape(str: string) {
+  return str
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;");
+}
+
+export function stringToColor(string: string) {
+  let hash = 0;
+  let i;
+
+  /* eslint-disable no-bitwise */
+  for (i = 0; i < string.length; i += 1) {
+    hash = string.charCodeAt(i) + ((hash << 5) - hash);
+  }
+
+  let color = "#";
+
+  for (i = 0; i < 3; i += 1) {
+    const value = (hash >> (i * 8)) & 0xff;
+    color += `00${value.toString(16)}`.slice(-2);
+  }
+  /* eslint-enable no-bitwise */
+
+  return color;
+}
+
+export function stringAvatar(name: string) {
+  if (!name) {
+    return {
+      sx: { bgcolor: "#000000" },
+      children: "?",
+    };
+  }
+  return {
+    sx: {
+      bgcolor: stringToColor(name),
+    },
+    children: `${name.slice(0, 2).toUpperCase()}`,
+  };
+}
